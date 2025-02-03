33f3d7d76032be84ef023fe52bba4eadfe2c2b79
Rama <glypthic@gmail.com>
2025-02-03 14:34:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## Installation Steps
+
+1. Copy ``.env.example`` and rename it to ``.env``
+
+
+2. Fill the environment credentials in .env
+
+3. Create ``env`` folder that will filled with python virtual environment
+
+4. Create virtual environment with this command:
+``python -m venv ./env``
+
+5. Install all required dependencies with this command:
+``pip install -r requirements.txt``
+
+3. Run the server with this command:
+``python main.py``
---
+++ main.py
@@ -0,0 +1,30 @@
+import grpc
+from concurrent import futures
+import proto.ai_service_pb2 as ai_service_pb2
+import proto.ai_service_pb2_grpc as ai_service_pb2_grpc
+from process.analyze import process
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AIService(ai_service_pb2_grpc.AIServiceServicer):
+    def AnalyzeData(self, request, context):
+        data = process(request)
+        print(os.environ)
+        return ai_service_pb2.DataResponse(
+            rotation_rpm=1600,
+            magnet_strength=0.8,
+            magnet_active=True
+        )
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ai_service_pb2_grpc.add_AIServiceServicer_to_server(AIService(), server)
+    server.add_insecure_port(f'[::]:{os.getenv('SERVER_PORT')}')
+    server.start()
+    print(f'Server Started at Port {os.getenv('SERVER_PORT')}')
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
